e20a61b6eb8c904fe6fa6287e0c44188472a1133
Gabe-T <gabriel.tarrant@hotmail.com>
2020-11-13 17:10:59 -0500

    api base url

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MoviesService } from '../movies.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  movieData: any;
 
-  constructor() { }
+  constructor(private router: Router, private route: ActivatedRoute, private service: MoviesService) { }
 
   ngOnInit(): void {
+    this.route.queryParamMap.subscribe((response) => {
+      let queryParams = response;
+        this.service
+          .getMovies(queryParams.get('term'))
+          .subscribe((response) => {
+            console.log(response);
+            this.movieData = response;
+          });
+      }
+    });
   }
+
+  search = (term: string) => {
+    this.router.navigate(['/home'], {
+      queryParams: {
+        term: term,
+      },
+    });
+  };
 
 }
--- src/app/movies.service.ts
+++ src/app/movies.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class MoviesService {
-movieBaseUrl: string = "https://api.themoviedb.org";
+movieBaseUrl: string = "https://api.themoviedb.org/3/search/movie";
 key: string = "86383bda503ee8f95b87e3d3c3499724";
+favorites: Favorites[] = [];
   constructor() { }
 }
